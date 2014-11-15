6f8fc1260f17d4b9216288009226bb0242bcb774
Andy Zinsser <andyznssr@gmail.com>
2014-11-14 18:41:20 -0800

    Add record model

--- input/models.py
+++ input/models.py
@@ -1,1 +1,16 @@
+from decimal import Decimal
 from django.db import models
+
+
+class Record(models.Model):
+    date = models.DateField(auto_now_add=True)
+    revenue = models.CharField(max_length=100)
+    dau = models.CharField(max_length=100)
+
+    @property
+    def arpu(self):
+        # Wrap in try in case revenue or dau was 0
+        try:
+            return '{0:.2f}'.format(Decimal(self.revenue) / Decimal(self.dau))
+        except:
+            return '0'
