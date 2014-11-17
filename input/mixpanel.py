7b4454274afb5832e5543c82788bc8d9ff924f72
Andy Zinsser <andyznssr@gmail.com>
2014-11-17 10:34:06 -0800

    Add revenue collected data to scraper

--- input/mixpanel.py
+++ input/mixpanel.py
@@ -18,17 +18,20 @@ A_WEEK = 7
 A_DAY = 1
 A_WEEK_AGO = '%s-%s-%s' % (TODAY.year, TODAY.month, TODAY.day - 7)
 YESTERDAY = '%s-%s-%s' % (TODAY.year, TODAY.month, TODAY.day - 1)
+TWO_DAYS_AGO = '%s-%s-%s' % (TODAY.year, TODAY.month, TODAY.day - 2)
 EXPIRE = int(time.time()) + 600
 
 
 class Scraper(object):
 
     def __init__(self):
-        # TODO:
-        #   setup a method for getting yesterdays dau
-        #   setup a method for getting yesterdays revenue
-        #   save as a new record
-        self.get_dau()
+        record, created = Record.objects.get_or_create(date=YESTERDAY)
+        logger.debug('created: %s' % created)
+        logger.debug('record: %s' % record)
+        record.dau = self.get_dau()
+        record.revenue = self.get_revenue()
+        record.save()
+        self.get_revenue()
         super(Scraper, self).__init__()
 
     def unicode_urlencode(self, params):
@@ -40,7 +43,6 @@ class Scraper(object):
         for i, param in enumerate(params):
             if isinstance(param[1], list):
                 params[i] = (param[0], json.dumps(param[1]),)
-
         return urllib.urlencode(
             [(k, isinstance(v, unicode) and v.encode('utf-8') or v) for k, v in params]
         )
@@ -52,23 +54,18 @@ class Scraper(object):
         for a in args:
             if isinstance(args[a], list):
                 args[a] = json.dumps(args[a])
-
         args_joined = ''
         for a in sorted(args.keys()):
             if isinstance(a, unicode):
                 args_joined += a.encode('utf-8')
             else:
                 args_joined += str(a)
-
             args_joined += '='
-
             if isinstance(args[a], unicode):
                 args_joined += args[a].encode('utf-8')
             else:
                 args_joined += str(args[a])
-
         hash = hashlib.md5(args_joined)
-
         if secret:
             hash.update(secret)
         elif API_SECRET:
@@ -87,22 +84,21 @@ class Scraper(object):
         }
         params['sig'] = self.hash_args(params)
         url = '/'.join([ENDPOINT, str(VERSION)]) + '/events/?' + self.unicode_urlencode(params)
-        logger.debug('url: %s' % url)
         r = requests.get(url)
-        logger.debug('YESTERDAY: %s' % YESTERDAY)
-        dau = r.json()['data']['values'][DAU_EVENT][YESTERDAY]
-        record, created = Record.objects.get_or_create(date=YESTERDAY)
-        record.dau = dau
-        record.save()
-        logger.debug('created: %s' % created)
-        logger.debug('record: %s' % record)
+        return r.json()['data']['values'][DAU_EVENT][YESTERDAY]
 
-    def save_as_record(self, data):
-        logger.debug('data:')
-        logger.debug(data)
-        # Go through each item
-        # Store in dict with keys by date
-        # then go through each day of the dict
-        # calculate the data points we want
-        # save it as a record
-        # then go back to testing the export
+    def get_revenue(self):
+        REVENUE_EVENT = 'Revenue Collected'
+        params = {
+            'api_key': API_KEY,
+            'expire': EXPIRE,
+            'event': REVENUE_EVENT,
+            'from_date': YESTERDAY,
+            'to_date': YESTERDAY,
+            'on': 'number(properties["amount"])',
+            'limit': 1000
+        }
+        params['sig'] = self.hash_args(params)
+        url = '/'.join([ENDPOINT, str(VERSION)]) + '/segmentation/sum?' + self.unicode_urlencode(params)
+        r = requests.get(url)
+        return r.json()['results'][YESTERDAY]
