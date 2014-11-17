954d7c235973c2c02ac567dafc04f28e9eeea1dd
Andy Zinsser <andyznssr@gmail.com>
2014-11-17 10:56:39 -0800

    Add url for staticfiles on prod

--- adapter/urls.py
+++ adapter/urls.py
@@ -1,7 +1,13 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.conf import settings
 
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^output/', include('output.urls')),
 )
+
+if not settings.DEBUG:
+    urlpatterns += patterns('',
+        (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
+    )
