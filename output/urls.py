ced863e0b48f189109897f1325d4b6c6482cd3db
Andy Zinsser <andyznssr@gmail.com>
2014-12-03 13:11:56 -0800

    Stub out algorithm ARPU endpoint

--- output/urls.py
+++ output/urls.py
@@ -6,4 +6,5 @@ urlpatterns = patterns('',
     url(r'^geckoboard/revenue-total', 'output.views.geckoboard_total_revenue', name='geckoboard_total_revenue'),
     url(r'^geckoboard/revenue', 'output.views.geckoboard_revenue', name='geckoboard_revenue'),
     url(r'^geckoboard/pool-impact', 'output.views.geckoboard_pool_impact', name='geckoboard_pool_impact'),
+    url(r'^geckoboard/algorithm-arpu', 'output.views.geckoboard_algorithm_arpu', name='geckoboard_algorithm_arpu'),
 )
